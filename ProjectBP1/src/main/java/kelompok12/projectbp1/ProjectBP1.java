ccaf4f241aa97113cf530621e25675a4a08d2e31
Asep Haryana <asepharyanalm001@gmail.com>
2024-12-13 18:28:06 +0700

    a

---
+++ ProjectBP1/src/main/java/kelompok12/projectbp1/ProjectBP1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package kelompok12.projectbp1;
+
+/**
+ *
+ * @author asephs
+ */
+public class ProjectBP1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
